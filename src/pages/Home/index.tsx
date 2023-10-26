c31b85913d97b7fee8f433c0902879dec6fbba7a
Cristiane Martins <crisplus@gmail.com>
2023-10-26 16:44:02 -0300

    Interface defaultValues. ntegração do react-hook-form ao TypeScript, adicionando interfaces para que o TypeScript entenda todos os campos existentes no nosso formulário de uma forma mais automatizada.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,13 +21,22 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
+interface NewCycleFormData {
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
-    console.log(data)
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(datas)
   }
 
   const task = watch('task')
