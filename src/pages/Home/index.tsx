02cef1b62b9aa5e11cb369d5c57f7fc8206d29d9
Cristiane Martins <crisplus@gmail.com>
2023-10-30 19:54:57 -0300

    Criação da variavel activeCycle para buscar o ciclo ativo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -57,6 +57,8 @@ export function Home() {
     reset()
   }
 
+  const activeCycle = cycles.find((cycle) => (cycle.id = activeCycleId))
+
   const task = watch('task')
   const isSubmitDisabled = !task
 
